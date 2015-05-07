9084f3dfcfc0bb07cb3c151ba379a9f1bc060942
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-05-07 20:10:24 +0100

    written failing tests for using deleting own review

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -56,4 +56,38 @@ feature 'reviewing' do
     leave_review('great', '5')
     expect(page).to have_content 'Average rating: ★★★★☆'
   end
+
+  # context 'users can only delete their own reviews' do
+
+  #   scenario 'logged out user' do
+  #     visit '/restaurants'
+  #     expect(page).not_to have_link 'Delete KFC'
+  #     restaurant = Restaurant.find_by_name('KFC')
+  #     visit "/restaurants/#{restaurant.id}/reviews/new"
+  #     expect(page).to have_content 'Users can only delete restaurants they have created!'
+  #   end
+
+  #   scenario 'logged in user' do
+  #     sign_up('test@test.com', 'testtest')
+  #     visit '/restaurants'
+  #     expect(page).not_to have_link 'Delete KFC'
+  #     restaurant = Restaurant.find_by_name('KFC')
+  #     visit "/restaurants/#{restaurant.id}/reviews/new"
+  #     expect(page).to have_content 'Users can only delete restaurants they have created!'
+  #   end
+  # end
+
+  scenario 'users can delete their own review' do
+    sign_up('test@test.com', 'testtest')
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: 'so so'
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    expect(current_path).to eq '/restaurants'
+    click_link 'Delete this review'
+    expect(page).to_have content 'Review deleted'
+    expect(page).not_to have_content 'so so'
+    expect(current_path).to eq '/restaurants'
+  end
 end
