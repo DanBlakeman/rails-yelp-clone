37f0b2559f817ee90d745eb6a2bf65a57bd3c1a5
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-05-08 16:40:12 +0100

    working with ajax

--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
   $('.endorsements-link').on('click', function(event) {
+    console.log('d')
 
     event.preventDefault();
 
     var endorsementCount = $(this).siblings('.endorsements_count');
 
     $.post(this.href, function(response) {
-      endorsementCount.text(resonse.new_endorsement_count);
+      endorsementCount.text(response.new_endorsement_count);
     })
 
   });
